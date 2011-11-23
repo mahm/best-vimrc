0706865e9450bda745ecbfa38d4eabffa292ffe8
mahm <nishimi@sonicgarden.jp>
2011-11-23 16:58:31 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestVimrc::Application.config.secret_token = '801ad76870c5503f8b9c09e6bb118cc2cd72ab12b8a51e7998112f356fd2a72d031270d091a25d341a45876a7520bbc9ffc32b5a0380126c5fbdcc637f5cf45f'
